ff8538e763821e8a2e31aac30271eba10df7fd8a
Lucas Sebe <lucassebe16@gmail.com>
2023-05-19 13:34:54 -0300

    add skeleton loading on img

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -6,7 +6,8 @@ import api from "../../services/api";
 import Btn from "../Button";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import digimonImg from "../../images/ff49c45e27b705edde662cb862936adc.jpg"
+import digimonImg from "../../images/ff49c45e27b705edde662cb862936adc.jpg";
+import { Skeleton } from "@mui/material";
 
 const theme = createTheme({
   palette: {
@@ -17,10 +18,10 @@ const theme = createTheme({
 export default function Input() {
   const [digimon, setDigimon] = useState("");
   const [value, setValue] = useState("");
-  const [imageUrl, setImageUrl] = useState(
-    digimonImg
-  );
+  const [imageUrl, setImageUrl] = useState(digimonImg);
   const [digimonid, setId] = useState("");
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     if (value !== "") {
       getDigimon();
@@ -29,10 +30,12 @@ export default function Input() {
 
   async function getDigimon() {
     try {
+      setLoading(true);
       const response = await api.get(`/${value}`);
       setDigimon(response.data);
-      setId(response.data)
+      setId(response.data);
       setImageUrl(response.data["images"][0]["href"]);
+      setLoading(false);
     } catch (error) {
       toast.error(`Digimon "${value}" não encontrado!`, {
         position: "top-right",
@@ -44,16 +47,26 @@ export default function Input() {
         progress: undefined,
         theme: "colored",
       });
+      setLoading(false);
     }
   }
 
   return (
     <ThemeProvider theme={theme}>
       <div className="image-container">
-        <img src={imageUrl} alt="guilmon-img" />
+        {loading ? (
+          <Skeleton
+            variant="rounded"
+            animation="wave"
+            width={350}
+            height={350}
+          />
+        ) : (
+          <img src={imageUrl} alt="guilmon-img" />
+        )}
       </div>
       <div id="nome">
-        <h2>{digimon && digimon.name + ' ID: ' + digimon.id}</h2>
+        <h2>{digimon && digimon.name + " ID: " + digimon.id}</h2>
       </div>
       <div id="input">
         <TextField
